00a13fe88be59ce5a3894d13d4c5a5cdbf7af802
1045446 <1045446@mborijnland.nl>
2022-09-06 14:24:58 +0200

    1e wijziging

---
+++ snake.js
@@ -0,0 +1,12 @@
+
+document.getElementById('snake');
+
+const canvas = document.getElementById("snake");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 400, 400);
+
+ctx.fillStyle = "white";
+ctx.fillRect(190, 190, 20, 20);
+
